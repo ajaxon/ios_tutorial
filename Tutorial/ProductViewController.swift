6de07a24145ab99521567a25e8dec3b1db081145
Allen <jaxon77@gmail.com>
2016-03-06 17:53:31 -0500

    outlets added

---
+++ Tutorial/ProductViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ProductViewController.swift
+//  Tutorial
+//
+//  Created by Allen on 3/6/16.
+//  Copyright © 2016 Allen. All rights reserved.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    @IBOutlet weak var productNameLabel: UILabel!
+    
+    @IBOutlet weak var productImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        productNameLabel.text = "1937 Desk Phone"
+        productImageView.image = UIImage(named: "phone-fullscreen3")
+
+    }
+
+
+
+}
